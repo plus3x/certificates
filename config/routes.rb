7598c6134f0d36c16757151df47334a763c80c24
Vladislav Petrov <electronicchest@gmail.com>
2013-07-26 01:02:10 +0400

    Configure home index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Certificates::Application.routes.draw do
+  
   get "home/index"
 
   resources :forms
-
-
   resources :categories
 
 
@@ -56,7 +55,7 @@ Certificates::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index', as: 'home'
 
   # See how all your routes lay out with "rake routes"
 
